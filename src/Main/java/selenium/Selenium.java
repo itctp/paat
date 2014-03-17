c77b77f5f7935bbe79d25de7751d85eff00cdf57
itctp <rakesh.ar@itcinfotech.com>
2014-03-17 14:29:15 +0530

    21

--- src/Main/java/selenium/Selenium.java
+++ src/Main/java/selenium/Selenium.java
@@ -66,7 +66,7 @@ public String getSessionId() {
 					 * sauce code
 					 */
 					DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-			        capabilities.setCapability("version", "24");
+			        capabilities.setCapability("version", "21");
 			        capabilities.setCapability("platform", Platform.XP);
 			        capabilities.setCapability("name",  testName.getMethodName());
 			        this.driver = new RemoteWebDriver(
